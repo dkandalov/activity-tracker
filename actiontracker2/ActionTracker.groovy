c9a1b35079a04871ed4266b691618ddde07b1bb7
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-12-28 12:12:36 +0000

    refactoring

--- actiontracker2/ActionTracker.groovy
+++ actiontracker2/ActionTracker.groovy
@@ -1,5 +1,4 @@
 package actiontracker2
-
 import com.intellij.ide.IdeEventQueue
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.ActionManager
@@ -7,26 +6,18 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.actionSystem.ex.AnActionListener
-import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.IdeFrame
 import com.intellij.openapi.wm.WindowManager
-import com.intellij.psi.PsiAnonymousClass
-import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiMethod
+import com.intellij.psi.*
+import com.intellij.util.Alarm
 import groovy.time.TimeCategory
 
-import javax.swing.SwingUtilities
-import java.awt.AWTEvent
+import java.awt.*
 import java.awt.event.KeyEvent
 import java.awt.event.MouseEvent
-import java.util.concurrent.atomic.AtomicReference
 
-import static liveplugin.PluginUtil.currentEditorIn
-import static liveplugin.PluginUtil.currentFileIn
-import static liveplugin.PluginUtil.currentPsiFileIn
-
+import static liveplugin.PluginUtil.*
+import static liveplugin.implementation.Misc.scheduleTask
 
 class ActionTracker {
 	private final TrackerLog trackerLog
@@ -35,55 +26,42 @@ class ActionTracker {
 		this.trackerLog = trackerLog
 	}
 
-	void startTracking(Disposable parentDisposable) {
+	void startTracking(Disposable parentDisposable, int trackIdeStateFrequencyMs = 1000) {
+		startIdeEventListeners(parentDisposable)
+		scheduleTask(new Alarm(Alarm.ThreadToUse.SWING_THREAD, parentDisposable), trackIdeStateFrequencyMs) {
+			trackerLog.append(captureIdeState())
+		}
+	}
+
+	private void startIdeEventListeners(Disposable parentDisposable) {
 		ActionManager.instance.addAnActionListener(new AnActionListener() {
 			@Override void afterActionPerformed(AnAction anAction, DataContext dataContext, AnActionEvent anActionEvent) {
 				def actionId = ActionManager.instance.getId(anAction)
-				trackerLog.append(createLogEvent(actionId))
+				trackerLog.append(captureIdeState(actionId))
 			}
-
 			@Override void beforeActionPerformed(AnAction anAction, DataContext dataContext, AnActionEvent anActionEvent) {}
-
 			@Override void beforeEditorTyping(char c, DataContext dataContext) {}
 		}, parentDisposable)
 
 		IdeEventQueue.instance.addPostprocessor(new IdeEventQueue.EventDispatcher() {
 			@Override boolean dispatch(AWTEvent awtEvent) {
 				if (awtEvent instanceof MouseEvent && awtEvent.getID() == MouseEvent.MOUSE_CLICKED) {
-					trackerLog.append(createLogEvent("MouseEvent:" + awtEvent.button + ":" + awtEvent.modifiers))
+					trackerLog.append(captureIdeState("MouseEvent:" + awtEvent.button + ":" + awtEvent.modifiers))
 
 					//			} else if (awtEvent instanceof MouseWheelEvent && awtEvent.getID() == MouseEvent.MOUSE_WHEEL) {
 					//				trackerLog.append(createLogEvent("MouseWheelEvent:" + awtEvent.scrollAmount + ":" + awtEvent.wheelRotation))
 
 				} else if (awtEvent instanceof KeyEvent && awtEvent.getID() == KeyEvent.KEY_PRESSED) {
 					//show("KeyEvent:" + (awtEvent.keyChar as int) + ":" + awtEvent.modifiers)
-					trackerLog.append(createLogEvent("KeyEvent:" + (awtEvent.keyChar as int) + ":" + awtEvent.modifiers))
+					trackerLog.append(captureIdeState("KeyEvent:" + (awtEvent.keyChar as int) + ":" + awtEvent.modifiers))
 				}
 				//			show(awtEvent)
 				false
 			}
 		}, parentDisposable)
-
-		// consider using com.intellij.openapi.actionSystem.impl.ActionManagerImpl#addTimerListener
-		def isDisposed = new AtomicReference<Boolean>(false)
-		new Thread({
-			while (!isDisposed.get()) {
-				AtomicReference logEvent = new AtomicReference<TrackerEvent>()
-				SwingUtilities.invokeAndWait {
-					logEvent.set(createLogEvent())
-				}
-				if (logEvent != null) trackerLog.append(logEvent.get())
-				Thread.sleep(1000)
-			}
-		} as Runnable).start()
-		Disposer.register(parentDisposable, new Disposable() {
-			@Override void dispose() {
-				isDisposed.set(true)
-			}
-		})
 	}
 
-	private static TrackerEvent createLogEvent(String actionId = "", Date time = now()) {
+	private static TrackerEvent captureIdeState(String actionId = "", Date time = now()) {
 		IdeFrame activeFrame = WindowManager.instance.allProjectFrames.find { it.active }
 		// this tracks project frame as inactive during refactoring
 		// (e.g. when "Rename class" frame is active)
