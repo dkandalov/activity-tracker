ac7f1eb14ec1bc37dfe492298a9e0ffb3768b802
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-12-28 11:59:28 +0000

    refactoring

--- actiontracker2/ActionTrackerPlugin.groovy
+++ actiontracker2/ActionTrackerPlugin.groovy
@@ -1,47 +1,39 @@
 package actiontracker2
-
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.util.Disposer
 import liveplugin.implementation.GlobalVar
 
 import static liveplugin.PluginUtil.newGlobalVar
+import static liveplugin.implementation.Misc.newDisposable
 
 class ActionTrackerPlugin {
 	private final ActionTracker tracker
 	private final Disposable pluginDisposable
 	private final GlobalVar<Boolean> isTracking
-	private final GlobalVar<Disposable> trackingDisposable
+	private Disposable trackingDisposable
 	PluginUI pluginUI
 
 	ActionTrackerPlugin(ActionTracker tracker, Disposable pluginDisposable) {
 		this.tracker = tracker
 		this.pluginDisposable = pluginDisposable
 		this.isTracking = newGlobalVar("ActionTrackerII.isTracking", false) as GlobalVar<Boolean>
-		this.trackingDisposable = newGlobalVar("ActionTrackerII.disposable") as GlobalVar<Disposable>
 	}
 
 	def onIdeStartup() {
 		isTracking.set(true)
-		tracker.startTracking(trackingDisposable.get())
+		trackingDisposable = newDisposable([pluginDisposable])
+		tracker.startTracking(trackingDisposable)
 		pluginUI.update(isTracking.get())
 	}
 
 	def toggleTracking() {
 		isTracking.set{ !it }
 		if (isTracking.get()) {
-			def disposable = new Disposable() {
-				@Override void dispose() {
-					isTracking.set(false)
-				}
-			}
-			Disposer.register(pluginDisposable, disposable)
-			trackingDisposable.set(disposable)
-
-			tracker.startTracking(disposable)
+			trackingDisposable = newDisposable([pluginDisposable])
+			tracker.startTracking(trackingDisposable)
 		} else {
-			def disposable = trackingDisposable.get()
-			if (disposable != null) {
-				Disposer.dispose(disposable)
+			if (trackingDisposable != null) {
+				Disposer.dispose(trackingDisposable)
 			}
 		}
 		pluginUI.update(isTracking.get())
