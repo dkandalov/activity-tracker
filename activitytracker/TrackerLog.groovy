d2e0dac2f9d68bd1490f0c83575054c3fea56976
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-12-30 21:26:55 +0000

    write events to file on a separate thread

--- activitytracker/TrackerLog.groovy
+++ activitytracker/TrackerLog.groovy
@@ -1,23 +1,47 @@
 package activitytracker
-
+import com.intellij.concurrency.JobScheduler
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.util.io.FileUtil
 import org.jetbrains.annotations.Nullable
 
 import java.text.SimpleDateFormat
+import java.util.concurrent.ConcurrentLinkedQueue
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS
+import static liveplugin.implementation.Misc.newDisposable
 
 class TrackerLog {
+	private final static long writeFrequencyMs = 1000
 	private final String statsFilePath
+	private final Disposable parentDisposable
+	private final Queue<TrackerEvent> eventQueue = new ConcurrentLinkedQueue<>()
 
-	TrackerLog(String path) {
+	TrackerLog(String path, Disposable parentDisposable) {
 		def pathFile = new File(path)
 		if (!pathFile.exists()) pathFile.mkdir()
 		this.statsFilePath = path + "/stats.csv"
+		this.parentDisposable = parentDisposable
+	}
+
+	def init() {
+		def runnable = {
+			def file = new File(statsFilePath)
+			def event = eventQueue.poll()
+			while (event != null) {
+				file.append(event.toCsv() + "\n")
+				event = eventQueue.poll()
+			}
+		} as Runnable
+		def future = JobScheduler.scheduler.scheduleAtFixedRate(runnable, writeFrequencyMs, writeFrequencyMs, MILLISECONDS)
+		newDisposable(parentDisposable) {
+			future.cancel(true)
+		}
+		this
 	}
 
 	def append(@Nullable TrackerEvent event) {
-		// TODO use queue
 		if (event == null) return
-		new File(statsFilePath).append(event.toCsv() + "\n")
+		eventQueue.add(event)
 	}
 
 	def resetHistory() {
