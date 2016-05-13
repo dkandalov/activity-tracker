a81bf5be84a06c4853c0ebaa02a2629b7ead2297
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-05-13 09:05:10 +0100

    refactoring GlobalVar

--- src/main/activitytracker/GlobalVar.kt
+++ src/main/activitytracker/GlobalVar.kt
@@ -30,46 +30,50 @@ class GlobalVar<T>(val id: String, value: T? = null, disposable: Disposable? = n
 
     companion object {
         val globalVarsKey: Key<ConcurrentHashMap<String, Any>> = Key.create("LivePlugin-GlobalVarsKey")
+        val keysByVarName = initKeysByVarName(globalVarsKey)
 
         private fun <T> getGlobalVar(varName: String, initialValue: T? = null): T? {
             return changeGlobalVar(varName, initialValue, {it})
         }
 
-        private fun <T> setGlobalVar(varName: String, varValue: T): T? {
-            return changeGlobalVar(varName){ varValue }
+        private fun <T> setGlobalVar(varName: String, varValue: T?): T? {
+            // explicit null parameter to make compiler not crash
+            return changeGlobalVar(varName, null as T?){ it -> varValue }
         }
 
-        private fun <T> changeGlobalVar(varName: String, initialValue: T? = null, callback: (T?) -> T): T? {
-            val application = ApplicationManager.getApplication()
-            var keysByVarName = application.getUserData(Companion.globalVarsKey)
-            if (keysByVarName == null) {
-                keysByVarName = ConcurrentHashMap<String, Any>()
-                application.putUserData(Companion.globalVarsKey, keysByVarName)
-            }
+        private fun <T> changeGlobalVar(varName: String, initialValue: T? = null, callback: (T?) -> T?): T? {
             @Suppress("UNCHECKED_CAST")
             val varValue = keysByVarName[varName] as T
 
             val prevValue = varValue ?: initialValue
             val newValue = callback(prevValue)
 
-            keysByVarName.put(varName, newValue as Any)
+            if (newValue == null) {
+                keysByVarName.remove(varName)
+            } else {
+                keysByVarName.put(varName, newValue as Any)
+            }
 
             return newValue
         }
 
         private fun <T> removeGlobalVar(varName: String): T? {
-            val application = ApplicationManager.getApplication()
-            var keysByVarName = application.getUserData(Companion.globalVarsKey)
-            if (keysByVarName == null) {
-                keysByVarName = ConcurrentHashMap<String, Any>()
-                application.putUserData(Companion.globalVarsKey, keysByVarName)
-            }
             @Suppress("UNCHECKED_CAST")
             val varValue = keysByVarName[varName] as T
 
             keysByVarName.remove(varName)
 
             return varValue
+        }
+
+        private fun initKeysByVarName(globalVarsKey: Key<ConcurrentHashMap<String, Any>>): ConcurrentHashMap<String, Any> {
+            val application = ApplicationManager.getApplication()
+            var keysByVarName = application.getUserData(globalVarsKey)
+            if (keysByVarName == null) {
+                keysByVarName = ConcurrentHashMap<String, Any>()
+                application.putUserData(globalVarsKey, keysByVarName)
+            }
+            return keysByVarName
         }
 
     }
