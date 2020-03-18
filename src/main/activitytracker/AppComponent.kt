4ffb6d9890d396f4712d205bb7f5704662aab60f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-03-18 23:57:29 +0000

    fix startup exception in the latest EAP: "Should be called at least in the state COMPONENTS_LOADED, the current state is: CONFIGURATION_STORE_INITIALIZED"

--- src/main/activitytracker/AppComponent.kt
+++ src/main/activitytracker/AppComponent.kt
@@ -1,18 +1,24 @@
 package activitytracker
 
+import com.intellij.ide.AppLifecycleListener
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.PathManager
 
 class AppComponent {
     init {
-        val disposable = ApplicationManager.getApplication()
-        val pathToTrackingLogFile = "${PathManager.getPluginsPath()}/activity-tracker/ide-events.csv"
+        val application = ApplicationManager.getApplication()
+        application.messageBus.connect().subscribe(AppLifecycleListener.TOPIC, object : AppLifecycleListener {
+            override fun appFrameCreated(commandLineArgs: MutableList<String>) {
 
-        val trackerLog = TrackerLog(pathToTrackingLogFile).initWriter(disposable, writeFrequencyMs = 10000L)
-        val tracker = ActivityTracker(trackerLog, disposable, logTrackerCallDuration = false)
-        val plugin = ActivityTrackerPlugin(tracker, trackerLog, PropertiesComponent.getInstance()).init()
-        val eventAnalyzer = EventAnalyzer(trackerLog)
-        PluginUI(plugin, trackerLog, eventAnalyzer, disposable).init()
+                val pathToTrackingLogFile = "${PathManager.getPluginsPath()}/activity-tracker/ide-events.csv"
+                val trackerLog = TrackerLog(pathToTrackingLogFile).initWriter(parentDisposable = application, writeFrequencyMs = 10000L)
+                val tracker = ActivityTracker(trackerLog, parentDisposable = application, logTrackerCallDuration = false)
+                val plugin = ActivityTrackerPlugin(tracker, trackerLog, PropertiesComponent.getInstance()).init()
+                val eventAnalyzer = EventAnalyzer(trackerLog)
+                PluginUI(plugin, trackerLog, eventAnalyzer, parentDisposable = application).init()
+
+            }
+        })
     }
 }
